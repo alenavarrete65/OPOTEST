4ed7e227f8add87e92310fd6dd35a7aa6414f2d9
alenavarrete65 <alenavarrete65@gmail.com>
2026-09-03 08:00:07 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Objetivo: que la app siempre abra (aunque no haya internet) y que puedas ver
    la última versión de tu banco de preguntas sincronizada, aunque no puedas guardar
    cambios nuevos hasta que vuelva la conexión (eso lo gestiona Firebase). */
-const CACHE_NAME = 'opotest-v1';
+const CACHE_NAME = 'opotest-v2';
 const APP_SHELL = [
   './',
   './index.html',
